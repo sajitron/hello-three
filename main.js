d04b15fba7c4b1a16272b8cfa367a24a4fff3b26
Adams Banjo <sajioloye@gmail.com>
2025-04-13 19:11:21 -0400

    set up linting

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 import * as THREE from "three";
-import WebGL from 'three/addons/capabilities/WebGL.js';
-
+import WebGL from "three/addons/capabilities/WebGL.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -22,16 +21,10 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-
-
-
-if ( WebGL.isWebGL2Available() ) {
-
+if (WebGL.isWebGL2Available()) {
   renderer.setAnimationLoop(animate);
-
-} else {
-
+}
+else {
   const warning = WebGL.getWebGL2ErrorMessage();
-  document.getElementById( 'container' ).appendChild( warning );
-
+  document.getElementById("container").appendChild(warning);
 }
